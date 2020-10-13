e2d686df10cb71bf8e791ba9d27b89864c88679d
EU3-USER <fedbidtest1@gmail.com>
2020-10-13 13:41:25 -0400

    added comments on same line

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("Sill really My Coworker added awesome hello"); // We are simulating the situation my coworker made this change 
+        System.out.println("This is my own hello"); // this is my local hello, and committing
 
     }
 

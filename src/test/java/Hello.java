f40dc1cbe1a25239d8059f55ca71cefae350b1a9
EU3-USER <fedbidtest1@gmail.com>
2020-10-13 13:45:58 -0400

    adding new line of comment locally ,not pushed yet

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Sill really My Coworker added awesome hello"); //We are simulating the situation my coworker made this change !!!
-
+        // adding some awesome local change
     }
 
 }

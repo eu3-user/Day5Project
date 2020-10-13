c6eb7963edc3868a0471a629198ddcf98b70280f
eu3-user <fedbidtest1@gmail.com>
2020-10-13 13:39:46 -0400

    added more changes by co-worker

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("Sill really awesome hello"); // added comment
+        System.out.println("Sill really My Coworker added awesome hello"); // We are simulating the situation my coworker made this change 
 
     }
 

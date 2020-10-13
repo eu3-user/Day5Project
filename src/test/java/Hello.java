2f16be555a0f4f6aaea0825028a1787959684bc8
gulgulyil <72104981+gulgulyil@users.noreply.github.com>
2020-10-13 19:28:46 +0200

    Update Hello.java

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Sill awesome hello");
+        
+        System.out.println("Forget hello")
 
     }
 

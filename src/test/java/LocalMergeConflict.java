71fc11fe4d8693720b87fc3479fc75118c317a48
EU3-USER <fedbidtest1@gmail.com>
2020-10-13 15:16:41 -0400

    adding initial code about conflict into master

---
+++ src/test/java/LocalMergeConflict.java
@@ -0,0 +1,12 @@
+public class LocalMergeConflict {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("added some awesome line in local master branch");
+
+
+    }
+
+
+}
